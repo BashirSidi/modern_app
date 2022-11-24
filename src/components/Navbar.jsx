acfaf5228f6eb01546862a30c21d332ac254027c
BashirSidi <bashirsalisusidi@gmail.com>
2022-11-24 18:59:29 +0100

    navbar mobile view on progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,40 @@
 import React, {useState} from 'react'
-
+import {navLinks} from '../constants'
+import {
+  close,
+  logo,
+  menu,
+} from '../assets'
 
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img 
+        src={logo} 
+        alt="logo" 
+        className='w-[124px] h-[32px]'
+      />
+
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((n, i) => (
+          <li
+            key={n.id}
+            className={`
+              font-poppins 
+              font-normal cursor-pointer
+              text-[16px]
+              text-white
+              ${i === navLinks.length - 1 ? 'mr-0' : 'mr-10'}
+            `}
+          >
+            <a href={`#${n.id}`}>
+              {n.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
   )
 }
 
